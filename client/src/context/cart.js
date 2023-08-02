03cfc6069da3abbfbb1e50596222d59c843a0777
AnuragVats007 <lit2020007@iiitl.ac.in>
2023-08-03 01:57:57 +0530

    cart edited

--- client/src/context/cart.js
+++ client/src/context/cart.js
@@ -6,32 +6,9 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    let res = localStorage.getItem("auth");
-    if (res) {
-      let userData = JSON.parse(res);
-      const idList = userData.user.cart;
 
-      // Use Promise.all to wait for all the promises to resolve
-      const cartItemsPromises = idList.map(async (item) => {
-        const p = await axios.get(
-          `${process.env.REACT_APP_API}/api/product/get-productbyid/${item}`
-        );
-        return p.data.product; // Return the product, not pushing into cartItems
-      });
-
-      // Use Promise.all to wait for all the promises to resolve
-      Promise.all(cartItemsPromises)
-        .then((cartItems) => {
-          setCart(cartItems); // Now you have the actual values in cartItems array
-          console.log(cartItems);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-
-    // let existingCartItem = localStorage.getItem("cart");
-    // if (existingCartItem) setCart(JSON.parse(existingCartItem));
+    let existingCartItem = localStorage.getItem("cart");
+    if (existingCartItem) setCart(JSON.parse(existingCartItem));
   }, [cart]);
 
   return (
